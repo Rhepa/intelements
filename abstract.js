f5dc0cc381ded95361623021555ad29c7487e17a
rika155 <3176897@ocadu.ca>
2021-06-16 20:15:31 -0400

    fffeaf

--- abstract.js
+++ abstract.js
@@ -1,4 +1,3 @@
-
 // DECLARING COLORS
 var lightblue ="#33C7F7";
 var pink="#DFA0C8";
@@ -6,26 +5,10 @@ var darkblue= "#2C439B";
 var violet= "#25255f";
 var yellow= "#e8b13b";
 
-var opacity
-
-
 // COLOR PALLETTE ARRAY
 var colorPalette= [lightblue, pink, lightblue, violet, yellow];
 var opacity=[]
 var randomColor = Math.floor(Math.random() *colorPalette.length); 
-
-// CHANGE BG COLOR
-// $("body").css("background-color",colorPalette[randomColor]);
-  
-  // // // DIV BOX SHOW/HIDE FUNCTION FRoM W3 SCHOOL
-  // var x = document.getElementById("circle-container");
-  // $("#box-container").append("<div></div>"); //ADD MORE BOXES
-  // if (x.style.display === "none") {
-  //   x.style.display = "block";
-  // } else {
-  //   x.style.display = "none";
-  // };
-
 
 //FOR LOOP
 for(var step = 10; step < 100; step++) {
@@ -34,13 +17,13 @@ for(var step = 10; step < 100; step++) {
 
 $("#circle-container div").each(function(){
   var randomColorBoxes = colorPalette[Math.floor(Math.random() *colorPalette.length)]; 
-$(this).css("background-color", randomColorBoxes);
+  $(this).css("background-color", randomColorBoxes);
 });
 
 //Random w & h
 $("#circle-container div").click(function(){
   var randomWidth = Math.random() * 200;
-  var randomHeight = Math.random() * 150;
+  var randomHeight = Math.random() * 200;
   $(this).css("width",randomWidth).css("height",randomHeight)
 });
 
@@ -53,15 +36,3 @@ $("#circle-container").ready(function() {
     $("#circle-container div").fadeIn();
   });
 });
-
-
-
-
-// function myFunction() {
-//   var x = document.getElementById("wholebox");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
